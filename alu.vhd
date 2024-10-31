3e063e9f6f5ae208a2e612fdc081a023f2cdfdb6
vini <frato.vini@gmail.com>
2024-10-31 20:13:10 -0300

    alu funcionando

--- alu.vhd
+++ alu.vhd
@@ -27,11 +27,15 @@ architecture Behaviour of alu is
 
     signal carry : STD_LOGIC_VECTOR (8 downto 0) := (others => '0');
     signal result : STD_LOGIC_VECTOR (7 downto 0) := (others => '0');
-    signal add_sub : STD_LOGIC := '0';
+    signal a_and_b : STD_LOGIC_VECTOR (7 downto 0) := (others => '0');
+    signal a_or_b : STD_LOGIC_VECTOR (7 downto 0) := (others => '0');
+    signal not_a : STD_LOGIC_VECTOR (7 downto 0) := (others => '0');  
 begin
-    --Como a instrução de subtração é em 1 podemos colocar a operação como o primeiro bit da operação ja q
-    --Ela vai ser feita de qualquer forma
-    add_sub <= op(0);
+    --A operação de subtração tem o ultimo bit 1
+    --O somador subtrator fica no modo subtração quando é o sinal 1 de operação
+    --Portanto podemos colocar o primeiro sinal do carry como 1 e o modo do somador subtrator
+    --como o primeiro bit da operação 
+    carry(0) <= op(0);
 
     ads : for i in 0 to 7 generate
         adsx: adder_sub
@@ -39,11 +43,15 @@ begin
             a => a(i),
             b => b(i),
             cin => carry(i),
-            a_b => add_sub,
+            a_b => op(0),
             q => result(i),
             cout => carry(i + 1)
         );
     end generate;
+
+    a_and_b <= a and b;
+    a_or_b <= a or b;
+    not_a <= not a;
 
     process (clk)
     begin
@@ -56,9 +64,9 @@ begin
                         flags(0) <= '0';
                     end if;
             
-                    flags(1) <= '0';
-                    flags(2) <= '0';
-                    flags(3) <= '0';
+                    flags(1) <= result(7);
+                    flags(2) <= carry(8);
+                    flags(3) <= carry(8) xor carry(7);
             
                     r <= result;
                 when "001" =>
@@ -68,13 +76,54 @@ begin
                         flags(0) <= '0';
                     end if;
             
-                    flags(1) <= '0';
+                    flags(1) <= result(7);
+                    flags(2) <= carry(8);
+                    flags(3) <= carry(8) xor carry(7);
+
+                    r <= result;
+                when "010" =>
+                    if (a_and_b = "00000000") then 
+                        flags(0) <= '1';
+                    else 
+                        flags(0) <= '0';
+                    end if;
+            
+                    flags(1) <= a_and_b(7);
                     flags(2) <= '0';
                     flags(3) <= '0';
 
-                    r <= result;
+                    r <= a_and_b;
+                when "011" =>
+                    if (a_or_b = "00000000") then 
+                        flags(0) <= '1';
+                    else 
+                        flags(0) <= '0';
+                    end if;
+            
+                    flags(1) <= a_or_b(7);
+                    flags(2) <= '0';
+                    flags(3) <= '0';
+
+                    r <= a_or_b;
+                when "100" =>
+                    if (not_a = "00000000") then 
+                        flags(0) <= '1';
+                    else 
+                        flags(0) <= '0';
+                    end if;
+            
+                    flags(1) <= not_a(7);
+                    flags(2) <= '0';
+                    flags(3) <= '0';
+                    
+                    r <= not_a;
                 when others =>
                     r <= "00000000";
+
+                    flags(0) <= '0';
+                    flags(1) <= '0';
+                    flags(2) <= '0';
+                    flags(3) <= '0';
             end case;
         end if;
     end process;    
